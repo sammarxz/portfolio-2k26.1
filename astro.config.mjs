b1025985e90802d1635969d31ea5e1d4f963a1bd
Sam Marxz <sammarxzz@gmail.com>
2026-03-02 14:34:12 -0300

    Add sitemap.xml generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,16 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
 import icon from 'astro-icon';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://sammarxz.com',
   vite: {
     plugins: [tailwindcss()],
   },
 
   integrations: [mdx(), react(), icon({
     iconDir: './src/assets/icons',
-  })],
+  }), sitemap()],
 });
